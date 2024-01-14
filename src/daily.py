0057a76b1d9ae93ad1587b27a887dff667b8bed0
Tom Blank <tblank@ieee.org>
2024-01-14 15:12:51 -0800

    fix to remove restore message if cold reboot

--- src/daily.py
+++ src/daily.py
@@ -1,3 +1,12 @@
+# Python program that starts at 6AM and displays then updates the daily calendar every ~6 hours
+# - 6AM, 12PM, 6PM, 12AM
+# - The calendar is displayed in full screen mode
+# The program is run on a Raspberry Pi 4B with connected to a TV monitor
+# Systemd starts the program at boot time
+
+
+import os
+print("DISPLAY:", os.environ.get('DISPLAY', 'Not Set'))
 import webbrowser
 import pyautogui
 from datetime import datetime
@@ -17,16 +26,22 @@ hour_of_day = 0
 minute_of_hour = 0
 
 def DisplayURL(url,Sleeping_sec):
-    # Open the URL in the default web browser
-    webbrowser.open_new(url)
+    global day_of_week, hour_of_day, minute_of_hour
+
+    # Get the default browser controller 
+    browser = webbrowser.get()
+
+    # Open the URL with the specified browser and parameters
+    browser.open("--app=" + url)
 
     # Wait for a short time to ensure the browser tab is fully loaded
     pyautogui.sleep(5)
 
     # Simulate pressing F11 to go full screen
     pyautogui.hotkey('F11')
+    pyautogui.sleep(Sleeping_sec)       #seconds sleep
 
-    pyautogui.sleep(Sleeping_sec)       #seconds
+    # Simulate pressing Ctrl-W to close the tab
     pyautogui.hotkey('ctrl', 'w')  #close tab   
     return
 
@@ -45,9 +60,25 @@ def Timing():
         print("---------------------------------")
     return
 
+def CalculateSleepTime():
+    # Calculate the number of seconds until the next update at 6AM, 12PM, 6PM, 12AM
+    global hour_of_day, minute_of_hour
+    
+    if hour_of_day < 6:
+        sleep_time = (6 - hour_of_day) * 3600
+    elif hour_of_day < 12:
+        sleep_time = (12 - hour_of_day) * 3600
+    elif hour_of_day < 18:
+        sleep_time = (18 - hour_of_day) * 3600
+    else:
+        sleep_time = (24 - hour_of_day + 6) * 3600
+    sleep_time = sleep_time - minute_of_hour * 60
+    if debug > 0:
+        print("Sleep time: " + str(sleep_time)) 
+    return sleep_time
+
 #main 
-# - Daily calls DisplayURL at 1AM every day of the week
-# - on Sunday also changes the display at 1PM
+# - Daily calls DisplayURL every 6 hours starting at 6AM
 def Main():
     global day_of_week, hour_of_day, minute_of_hour, URLS
     SIXHOURS = 21600    #seconds
@@ -56,13 +87,12 @@ def Main():
         Timing()
         url = URLS[day_of_week]
         print("URL: " + url)
-        DisplayURL(url, SIXHOURS)   #seconds
+        #Show diplay until next update time - 6AM, 12PM, 6PM, 12AM
+        DisplayURL(url, CalculateSleepTime()+70)    #In case of a delay, add 70 seconds insuring midnight update is on next day
 
-
-    
 
 __name__ = "__main__"
-debug = 1
+debug = 0
 if debug > 0:
     for tmp_day in range(0, 7):
         day_of_week = tmp_day
